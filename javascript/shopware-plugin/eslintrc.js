6e87f25c6d5a5b2317b1f620f1eeea0d031a09fe
Thomas Nordquist <thomas.nordquist@viison.com>
2017-07-06 18:58:27 +0200

    Propose eslint changes

--- javascript/shopware-plugin/eslintrc.js
+++ javascript/shopware-plugin/eslintrc.js
@@ -6,4 +6,10 @@ module.exports = {
         // global Ext object should not be written
         'Ext': false,
     },
+    rules: {
+        'spaced-comment': 0,
+        'comma-dangle': 0, // trailing comma after last element of array / object
+        'space-before-function-paren': 0, // function(){} => function (){}
+        'vars-on-top': 0 // Declaration on top of scope
+    }
 };
