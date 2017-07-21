a705d19dec5b3c937a31226097cd52bac7f02d9c
Jannik Jochem <jannik.jochem@viison.com>
2017-07-21 15:28:52 +0200

    Add ESLint to pre-commit hook

--- javascript/shopware-plugin/eslintrc.js
+++ javascript/shopware-plugin/eslintrc.js
@@ -3,7 +3,13 @@
 module.exports = {
     'extends': '../legacy/eslintrc.js',
     'globals': {
-        // global Ext object should not be written
         'Ext': false,
+        'ViisonCurrencyFormatter': false,
+        'Shopware': false,
+    },
+    rules: {
+        // Downgrade max-len to warning since this is impractical with ExtJS's protracted class names
+        'max-len': [1, 120],
+        'consistent-this': [1, 'actuallyWeDontAllowThisAtAll'],
     },
 };
