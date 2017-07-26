0ee53497da4f1e59be5f9fbf6525132418501bd0
Jannik Jochem <jannik.jochem@viison.com>
2017-07-26 17:44:55 +0200

    Add no-console rule to plugin eslint config

--- javascript/shopware-plugin/eslintrc.js
+++ javascript/shopware-plugin/eslintrc.js
@@ -11,5 +11,6 @@ module.exports = {
         // Downgrade max-len to warning since this is impractical with ExtJS's protracted class names
         'max-len': [1, 120],
         'consistent-this': [1, 'actuallyWeDontAllowThisAtAll'],
+        'no-console': 'error',
     },
 };
