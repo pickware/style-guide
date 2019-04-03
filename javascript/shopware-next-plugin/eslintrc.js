4a3ad2dd5862554da48333f573e3bfbe22e3640f
Jannik Jochem <jannik.jochem@pickware.com>
2019-04-03 18:28:50 +0200

    Add support for Shopware Next plugins

---
+++ javascript/shopware-next-plugin/eslintrc.js
@@ -0,0 +1,11 @@
+/* eslint-disable quote-props */
+
+module.exports = {
+    'extends': '../eslintrc.js',
+    'globals': {
+        'Shopware': false,
+    },
+    rules: {
+        'no-console': 'error',
+    },
+};
