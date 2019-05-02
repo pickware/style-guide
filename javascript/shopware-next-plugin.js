df1525ef9e91e7802e1e63c924cbd8db2f6d97c5
Jannik Jochem <jannik.jochem@pickware.com>
2019-05-02 13:26:58 +0200

    Add eslint-plugin-promise (#34)

--- javascript/shopware-next-plugin.js
+++ javascript/shopware-next-plugin.js
@@ -9,11 +9,16 @@ module.exports = {
         'airbnb-base/rules/variables',
         'airbnb-base/rules/es6',
         require.resolve('./pickware-rules.js'),
+        require.resolve('./pickware-modern-rules.js'),
     ],
     'rules': {
         'no-console': 'error',
+        // Disable rules that prohibit manual callback/promise interoparability code until we have a build process for
+        // Shopware plugins which can ship an interoperability library (e.g. pify).
+        'promise/no-callback-in-promise': 'off',
+        'promise/avoid-new': 'off',
     },
-    parserOptions: {
+    'parserOptions': {
         ecmaVersion: 2018,
     },
 };
