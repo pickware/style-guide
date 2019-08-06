81641a19011baf6b240d1046f5243bf8de0862c8
rebekkaorth <43952346+rebekkaorth@users.noreply.github.com>
2019-08-06 10:24:44 +0200

    Add script for cleaning up during CI builds of Swift projects (#37)
    Related to VIISON/POSApp#411

---
+++ swift/ci-cleanup.swift
@@ -0,0 +1,14 @@
+#!/usr/bin/swift
+
+import Foundation
+
+func removeIfExists(atPath path: String) throws {
+	if FileManager.default.fileExists(atPath: path) {
+		print("Removing \(path)")
+		try FileManager.default.removeItem(atPath: path)
+	}
+}
+
+try removeIfExists(atPath: "./.bundle")
+try removeIfExists(atPath: "./.swiftlint-ci.yml")
+try removeIfExists(atPath: "./Frameworks/.swiftlint.yml")
