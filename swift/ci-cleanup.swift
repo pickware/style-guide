a9364512a169501376f36b85316358eb7a78b061
Sven Münnich <sven.muennich@viison.com>
2019-08-07 14:22:51 +0200

    Convert tabs to spaces

--- swift/ci-cleanup.swift
+++ swift/ci-cleanup.swift
@@ -3,10 +3,10 @@
 import Foundation
 
 func removeIfExists(atPath path: String) throws {
-	if FileManager.default.fileExists(atPath: path) {
-		print("Removing \(path)")
-		try FileManager.default.removeItem(atPath: path)
-	}
+    if FileManager.default.fileExists(atPath: path) {
+        print("Removing \(path)")
+        try FileManager.default.removeItem(atPath: path)
+    }
 }
 
 try removeIfExists(atPath: "./.bundle")
