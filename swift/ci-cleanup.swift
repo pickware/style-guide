018f4d3f8a138652379eb373d4790be2063c7f05
rebekkaorth <rebekka.orth@viison.com>
2019-08-13 13:56:06 +0200

    Fix access control level error in CI

--- swift/ci-cleanup.swift
+++ swift/ci-cleanup.swift
@@ -2,7 +2,7 @@
 
 import Foundation
 
-func removeIfExists(atPath path: String) throws {
+public func removeIfExists(atPath path: String) throws {
     if FileManager.default.fileExists(atPath: path) {
         print("Removing \(path)")
         try FileManager.default.removeItem(atPath: path)
