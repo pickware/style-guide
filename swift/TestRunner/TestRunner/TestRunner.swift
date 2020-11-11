ad76323a19cdf819a5f5609c1540ca43bfd8ef36
Sven Münnich <sven.muennich@pickware.de>
2020-11-11 09:03:22 +0100

    Fix code style of Swift test runner

--- swift/TestRunner/TestRunner/TestRunner.swift
+++ swift/TestRunner/TestRunner/TestRunner.swift
@@ -3,11 +3,6 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-    func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
-        return true
-    }
+internal class AppDelegate: UIResponder, UIApplicationDelegate {
+    internal func applicationDidFinishLaunching(_ application: UIApplication) {}
 }
