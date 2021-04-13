97353d0d99bc5e3186c5b97fcb7a15829f2f71d4
Niklas Büchner <31041526+niklasbuechner@users.noreply.github.com>
2021-04-13 12:01:10 +0200

    Add a sniff for method naming with testdox for SW6 plugins (#71)

---
+++ php/php-codesniffer-standard/VIISON/Tests/Methods/UnitTestMethodNameUnitTest.php
@@ -0,0 +1,33 @@
+<?php
+namespace VIISON\StyleGuide\PHPCS\Standards\VIISON\Tests\Methods;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+
+class UnitTestMethodNameUnitTest extends AbstractSniffUnitTest
+{
+    /**
+     * @inheritdoc
+     */
+    public function getErrorList($testFile = 'UnitTestMethodNameUnitTest.inc')
+    {
+        switch ($testFile) {
+            case 'UnitTestMethodNameUnitTest.inc':
+                return [
+                    8 => 1,
+                    16 => 1,
+                    24 => 1,
+                    32 => 1,
+                ];
+            default:
+                return [];
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getWarningList()
+    {
+        return [];
+    }
+}
