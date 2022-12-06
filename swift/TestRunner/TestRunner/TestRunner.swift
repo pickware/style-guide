19335bdf7652aa80e4501f4474717151818632cb
Sven Münnich <sven.muennich@pickware.de>
2022-12-06 11:26:02 +0100

    Remove SwiftLint config (#83)

--- swift/TestRunner/TestRunner/TestRunner.swift
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Pickware GmbH. All rights reserved.
-
-import UIKit
-
-@UIApplicationMain
-internal class AppDelegate: UIResponder, UIApplicationDelegate {
-    internal func applicationDidFinishLaunching(_ application: UIApplication) {}
-}
