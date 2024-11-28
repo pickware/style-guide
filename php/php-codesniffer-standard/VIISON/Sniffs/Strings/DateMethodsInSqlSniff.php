ad0050ee790117e51e73b1ceac41288cc62420f3
Manuel Kress <6232639+windaishi@users.noreply.github.com>
2024-11-28 16:40:38 +0100

    WIP

--- php/php-codesniffer-standard/VIISON/Sniffs/Strings/DateMethodsInSqlSniff.php
+++ php/php-codesniffer-standard/VIISON/Sniffs/Strings/DateMethodsInSqlSniff.php
@@ -4,11 +4,14 @@ namespace VIISON\StyleGuide\PHPCS\Standards\VIISON\Sniffs\Strings;
 use PHP_CodeSniffer\Files\File;
 use PHP_CodeSniffer\Sniffs\Sniff;
 
+// phpcs:disable VIISON.Strings.DateMethodsInSqlSniff.NoNow
+// phpcs:disable VIISON.Strings.DateMethodsInSqlSniff.NoCurrentTimestamp
+
 /**
  * This sniff disallows the usage of the method NOW() and CURRENT_TIMESTAMP() in SQL queries as they have weird
  * timezone behavior. It suggests using UTC_TIMESTAMP() instead.
  */
-class NoNowInMySqlSniff implements Sniff
+class DateMethodsInSqlSniff implements Sniff
 {
     /**
      * @inheritdoc
@@ -32,12 +35,17 @@ class NoNowInMySqlSniff implements Sniff
 
         if (stripos($content, 'NOW(') !== false) {
             $error = 'The usage of the method NOW() in SQL queries is not allowed. Use UTC_TIMESTAMP() instead.';
-            $phpcsFile->addError($error, $stackPtr, 'NoNowInSql');
+            $phpcsFile->addError($error, $stackPtr, 'NoNow');
         }
 
         if (stripos($content, 'CURRENT_TIMESTAMP(') !== false) {
             $error = 'The usage of the method CURRENT_TIMESTAMP() in SQL queries is not allowed. Use UTC_TIMESTAMP() instead.';
-            $phpcsFile->addError($error, $stackPtr, 'NoCurrentTimestampInSql');
+            $phpcsFile->addError($error, $stackPtr, 'NoCurrentTimestamp');
+        }
+
+        if (preg_match('/UTC_TIMESTAMP\\(\\s*?[012456789]?\\s*?\\)/', $content)) {
+            $error = 'Always pass 3 as an argument for UTC_TIMESTAMP() to ensure a consistent precision.';
+            $phpcsFile->addError($error, $stackPtr, 'UtcTimestampOnlyWithArgument');
         }
     }
 }
